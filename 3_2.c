0617a4564ac5fdd13265a0f28db276eef046bd77
waqarwasif <waqarwasif0333@gmail.com>
2025-09-27 20:18:38 +0500

    changed

--- 3_2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-    int x=2;
-    x=x+1;
-    x+=1;
-    ++x;
-    x++;
-}
--- 3_4.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-
-    int sum,x;
-    x=1;
-    sum=0;
-    sum+=x;
-    printf("The sum is: %d",sum);
-}
